625c3f860f1b9a10fa9315900e609aebe27b7c87
Phil <102290118+Phil0s@users.noreply.github.com>
2022-06-25 16:08:19 +1000

    Mvvm Notes

--- JollyCooks/JollyCooksApp.swift
+++ JollyCooks/JollyCooksApp.swift
@@ -4,6 +4,12 @@
 //
 //  Created by Phillip Shen on 15/3/22.
 //
+/* Mvvm Notes
+    Model - Data
+    View - UI
+    ViewModel - manages Models for View (Like puts/connects them together).
+*/
+
 
 import SwiftUI
 
