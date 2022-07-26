0d5c407b225bb0d39118a177f16f58d143bc5e62
Phil <102290118+Phil0s@users.noreply.github.com>
2022-07-26 18:32:01 +1000

    Added a light and dark mode. Not assist collar but fixed collar with brightness altered. Improved settings page UI

---
+++ JollyCooks/Views/Settings/EmailView.swift
@@ -0,0 +1,20 @@
+//
+//  EmailView.swift
+//  JollyCooks
+//
+//  Created by Phillip Shen on 26/7/2022.
+//
+
+import SwiftUI
+
+struct EmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmailView()
+    }
+}
--- JollyCooks/Views/Settings/SettingsView.swift
+++ JollyCooks/Views/Settings/SettingsView.swift
@@ -21,17 +21,16 @@ struct SettingsView: View {
                     } label: {
                         Text("\(Image(systemName:"person.crop.circle.fill.badge.minus")) Log Out")
                     }
-                    
-                    
-//                        .listRowBackground(Color.orange)
-//                    Button(action: testing, label: Text(""))
                     Text("\(Image(systemName: "power")) Deactivate")
                 }
+                .listRowBackground(colorScheme == .dark ? Color.black.brightness(0.2) : Color.gray.brightness(0.43))
+                
                 
                 
                 Section(header: Text("About Us")) {
                     Text("\(Image(systemName: "questionmark.circle")) About JollyCooks")
                 }
+                .listRowBackground(colorScheme == .dark ? Color.black.brightness(0.2) : Color.gray.brightness(0.43))
                 
                 Section(header: Text("Meet the team")){
                     NavigationLink {
@@ -39,52 +38,65 @@ struct SettingsView: View {
                         
                     } label: {
                         PersonThree()
-                    }
+                    }.listRowBackground(colorScheme == .dark ? Color.black.brightness(0.2) : Color.gray.brightness(0.43))
                  
                     NavigationLink {
                         AadityaInfo()
                         
                     } label: {
                         PersonTwo()
-                    }
+                    }.listRowBackground(colorScheme == .dark ? Color.black.brightness(0.2) : Color.gray.brightness(0.43))
                     
                     NavigationLink {
                         AndrewInfo()
                         
                     } label: {
                        PersonOne()
-                    }
+                    }.listRowBackground(colorScheme == .dark ? Color.black.brightness(0.2) : Color.gray.brightness(0.43))
                     
                     NavigationLink {
                         JoeInfo()
                         
                     } label: {
                         PersonFour()
-                    }//sorry joe the image I had, had only three male avatar and I wasn't bothered to find a new palatte of avatars  so I just gave you a female avatar.
+                    }.listRowBackground(colorScheme == .dark ? Color.black.brightness(0.2) : Color.gray.brightness(0.43))
+                    
+                    //sorry joe the image I had, had only three male avatar and I wasn't bothered to find a new palatte of avatars  so I just gave you a female avatar.
                 }
                 
                 NavigationLink {
-                    PhillipInfo()
+                    EmailView()
                     
                 } label: {
                     Text("\(Image(systemName: "headphones")) Contact Us")
                     .foregroundColor(Color.red)
                 }
+                .listRowBackground(colorScheme == .dark ? Color.black.brightness(0.2) : Color.gray.brightness(0.43))
                 
             }// ENd of List
+            .onAppear {
+                // Set the default to clear
+                UITableView.appearance().backgroundColor = .clear
+            }
             .listStyle(.insetGrouped)
             .navigationTitle("Settings")
-                
-         
             
         } //Navigation view end
         .navigationViewStyle(StackNavigationViewStyle())
+        
+      
     }
+    
         
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        Group{
+            SettingsView()
+
+            SettingsView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
