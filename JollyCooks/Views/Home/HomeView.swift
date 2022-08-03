ed7a30e0162f722cf2c5a9418ca4dd7618dba872
bucL <Shankar_Aa@student.kings.edu.au>
2022-08-03 21:44:22 +1000

    "Finished" home view for Joe because he was assigned this but like was too lazy to do anything so yeah this is just an image rip our marks.

--- JollyCooks/Views/Home/HomeView.swift
+++ JollyCooks/Views/Home/HomeView.swift
@@ -3,21 +3,20 @@
 //  JollyCooks
 //
 //  Created by Phillip Shen on 22/7/2022.
-//
+// This view is literally an image because Joe was supposed to do this but then at the last minute i made the decision that if
+// I had the time I would do it but we don't have timne at this point so yeah -- Aaditya.
 
 import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationView{
-            ScrollView{
-                Text("This is the home page")
-            }
+            Image("homeview") // Dummy model for the hoemview because we didn't have enough time to finish this and Joe didn't do it cause he's lazy af. 
+            .resizable()
+            .frame(width: 820, height: 1136)
+            .offset(y:-20)
         }
-        .navigationTitle("Recipes")
-        .navigationViewStyle(StackNavigationViewStyle())
+        
     }
-}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
